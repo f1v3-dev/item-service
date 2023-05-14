011bed950a0e4a5b6aac183d8259733f06912a06
f1v3-dev <f1v3_dev@naver.com>
2023-05-14 22:39:11 +0900

    [RedirectAttributes - add 확인]

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -87,7 +88,7 @@ public class BasicItemController {
     /**
      * PRG - Post/Redirect/Get
      */
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV5(Item item) {
 
         itemRepository.save(item);
@@ -95,6 +96,17 @@ public class BasicItemController {
 
     }
 
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        
+        return "redirect:/basic/items/{itemId}"; // -> (@GetMapping("/{itemId}") 컨트롤러 호출
+        // ?status=와 같이 쿼리 파라미터로 들어감
+
+    }
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
